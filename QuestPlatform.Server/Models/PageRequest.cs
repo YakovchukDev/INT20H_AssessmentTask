f5f5a0156d239b21cf13617b59dada9f8a07d225
DimaBohdan <bohdan777.dmytro@gmail.com>
2025-02-09 13:18:16 +0200

    add dtos, QuestController, QuestService

---
+++ QuestPlatform.Server/Models/PageRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace QuestPlatform.Server.Models
+{
+    public class PageRequest
+    {
+        public int PageNumber { get; set; }
+        public string Title { get; set; }
+        public List<PageElementRequest> PageElements { get; set; }
+        public List<CreateTaskRequest> Tasks { get; set; }
+    }
+}
